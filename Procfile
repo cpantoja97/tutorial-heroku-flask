fd33545fe28e4ee5b01b2c28a86bf63793d658d6
Camila Pantoja <c.pantoja@uniandes.edu.co>
2022-05-08 13:21:43 -0500

    fix paths

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn app:app
-worker: celery -A src.celery.tasks.app worker
+worker: celery -A tasks.app worker
--- __init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-# Adding the 'src' directory to the python path
-sys.path.insert(0, os.path.join(os.getcwd(), 'src'))
